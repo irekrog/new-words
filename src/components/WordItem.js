abb583b8bdd3fd9678158494c3cd9dbfb3e120e9
irekrog <irekfrontend@gmail.com>
2017-02-13 14:08:18 +0100

    show dialog with edit item

--- src/components/WordItem.js
+++ src/components/WordItem.js
@@ -6,18 +6,32 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import {grey400} from 'material-ui/styles/colors';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+
+import '../sass/item.sass';
 
 export default class WordItem extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      listData: []
+      listData: [],
+      openEdit: false,
+      wordEdit: '',
+      definitionEdit: ''
     };
+
+    this.getEditWord = this.getEditWord.bind(this);
+    this.getEditDefinition = this.getEditDefinition.bind(this);
+
+    this.getItems();
   }
 
   componentDidMount() {
-    this.getItems();
+
   }
 
   getItems() {
@@ -42,16 +56,52 @@ export default class WordItem extends Component {
     word.remove();
   }
 
+  editItem(itemKey, editWord, editDefinition) {
+    this.setState({
+      openEdit: true,
+      editWord,
+      editDefinition
+    });
+    console.log(itemKey);
+  }
+
+  getEditWord(e) {
+    this.setState({
+      editWord: e.target.value
+    });
+  }
+
+  getEditDefinition(e) {
+    this.setState({
+      editDefinition: e.target.value
+    });
+  }
+
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={() => { this.setState({openEdit: false}); }}
+      />,
+      <RaisedButton
+        label="Save"
+        secondary={true}
+        onTouchTap={() => { this.setState({openEdit: false}); }}
+      />
+    ];
+
     const iconButtonElement = (
       <IconButton touch={true}>
         <MoreVertIcon color={grey400}/>
       </IconButton>
     );
 
-    const rightIconMenu = (key) => (
+    const rightIconMenu = (key, word, definition) => (
       <IconMenu iconButtonElement={iconButtonElement}>
-        <MenuItem>Edit</MenuItem>
+        <MenuItem onTouchTap={() => {
+          this.editItem(key, word, definition);
+        }}>Edit</MenuItem>
         <MenuItem onTouchTap={() => {
           this.removeItem(key);
         }}>Delete</MenuItem>
@@ -62,16 +112,40 @@ export default class WordItem extends Component {
       .map(item => {
         return (
           <ListItem
+            className="word-item"
             primaryText={item.word}
             secondaryText={item.definition}
-            rightIconButton={rightIconMenu(item.key)}
+            rightIconButton={rightIconMenu(item.key, item.word, item.definition)}
             id={item.key}
           />
         );
       });
 
     return (
-      <List>{items}</List>
+      <div>
+        <List>{items}</List>
+        <Dialog
+          title="Edit your item"
+          modal={false}
+          open={this.state.openEdit}
+          actions={actions}
+          onRequestClose={() => { this.setState({openEdit: false}); }}
+        >
+          <TextField
+            hintText="Edit your word"
+            onChange={this.getEditWord}
+            value={this.state.editWord}
+            fullWidth={true}
+          />
+          <br />
+          <TextField
+            hintText="Enter your definition"
+            onChange={this.getEditDefinition}
+            value={this.state.editDefinition}
+            fullWidth={true}
+          />
+        </Dialog>
+      </div>
     );
   }
 }
